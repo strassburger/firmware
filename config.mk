d6e81bc2c554d4e1195b086f079531c59e49ffef
Sven Roederer <devel-sven@geroedel.de>
2017-07-15 12:47:16 +0200

    packages: add variant for uplink w/o tunnel
    as there ist currently no way to dynamically select
    an uplink-provider

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
+PACKAGES_LIST_DEFAULT=default notunnel tunnel-berlin backbone
 OPENWRT_SRC=https://github.com/openwrt/openwrt.git
 OPENWRT_COMMIT=dca4dfacf22e93017c90c308dbeec27a77b4f338
 MAKE_ARGS=

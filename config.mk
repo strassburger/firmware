ca5b5e6de631504c7de4218bf3280bcabc61dee9
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-11-01 16:18:44 +0100

    config: switch to LEDE-master (27 Oct 2017)

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=367b4563b4cd466188bd4c8cf24ff0a125029c1c
+LEDE_COMMIT=098afa1e1b29918c8d9d4a675464be896aadc7a1
 MAKE_ARGS=

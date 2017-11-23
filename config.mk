9d04cc9845ffd676b4292cbb6c64cfc2000f26c2
Sven Roederer <freifunk@it-solutions.geroedel.de>
2017-11-23 10:13:02 +0100

    update LEDE to HEAD of master (Nov 24, 2017)
    9fe59ab kernel: bump 4.9 to 4.9.65
    088262a wireguard: bump to 20171122
    332438b uboot-sunxi: fix build of HAOYU Electronics Marsboard A10
    b5d52b2 sunxi: remove support for kernel 4.4
    c048ca0 sunxi: backport sunxi-mmc controller driver from 4.13
    4e93ec1 kernel: i2c-piix4: fix dependency on TARGET_x86
    9cfcd49 kernel: e100: take e100 firmware from linux-firmware repository
    06e1b90 kernel: add NFS4 client support
    9f13f98 kernel: Hyper-V PCI pass through
    551de61 kernel: add 32bit x86 HYPER-V support
    0c78de8 kernel: add kmod-i2c-i801
    7ffeecd kernel: add it87-wdt watchdog timer module
    62ede4f kernel: bump 4.9 to 4.9.63
    9052dd6 libusb-compat: Upgrade to 0.1.15
    08cc9a2 tools/e2fsprogs: Update to 1.43.7
    fc4e7bd usbutils: Update usb.ids file to latest
    5e6792e wireless-regdb: fix PKG_MIRROR_HASH
    298ade2 acx-mac80211: fix build on kernel 4.9
    aa82141 ramips: use pinmux nodes from dtsi
    b69ceb0 ramips: add missing pinmuxes to SoC dtsi
    e142173 ramips: backport MT7628 pinmux fixes
    2f9fe78 ramips: fix Planex CS-QR10 device packages
    a783185 ramips: fix DCH-M225 support
    6aa4b97 odhcpd: fix gcc7 build error
    6346291 hostapd: remove unused local var declaration
    cc0847e hostapd: don't set htmode for wpa_supplicant
    a28d1d5 odhcpd: update to latest git HEAD (make dhcpv4 support optional)
    6a6dc94 mac80211: fix a race condition that could lead to a use-after-free on a timer
    9247864 mac80211: fix netlink family id for nl80211 messages
    b9e700c kernel: fix lzo and lz4 modules
    bc50a97 dnsmasq: load instance-specific conf-file if exists
    bd1ee90 wolfssl: add PKG_CPE_ID ids to package and tools
    dca96b7 openssl: Add optimization option
    358df5e ixp4xx: Update to 4.9 kernel, refresh patches. Remove kmod-spi-gpio-old usage.
    6fcf422 netifd: update to latest git HEAD

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 LEDE_SRC=https://git.lede-project.org/source.git
-LEDE_COMMIT=370ce1fb48ea8b5b8fc2f91ed536a4f306fa9c82
+LEDE_COMMIT=9fe59abef8bde3b7f32868873c4e1f76e9222d46
 MAKE_ARGS=

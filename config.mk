709f9abaa26f6017176a4f42e8168e8fd78eda3b
Sven Roederer <devel-sven@geroedel.de>
2018-01-07 17:05:23 +0100

    switch to primary OpenWRT Repo-URL
    The real repository is hosted on https://git.openwrt.org,
    Github just get's synced to this.

--- config.mk
+++ config.mk
@@ -2,6 +2,6 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
-OPENWRT_SRC=https://github.com/openwrt/openwrt.git
+OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=7aa15953e1b60033fb1390b97fd6fe42daced738
 MAKE_ARGS=

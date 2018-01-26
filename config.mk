2062f137f002b0cf404f0f9d8ed41922d17247b7
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-01-27 00:24:57 +0100

    update to HEAD of OpenWRT-master (26. Jan 2018)
    d58c8f4029 mac80211: revert "wireless: set correct mandatory rate flags"
    cfdfa6d04e rules.mk: drop `include_mk` build rule
    2c50af0cea openssl: tell the build system that we are doing CROSS_COMPILE
    e6de92cdcc iptables: make kmod-ipt-debug part of default ALL build
    dc555d003c build: disable BUILD_PATENTED by default
    03a00eeab3 wireguard: bump to 20180118
    6271539fc5 Revert "mt76: update to the latest version"
    99eb128aca mt76: update to the latest version
    4ba2dd130a ar71xx: add ew-balin platform from Embedded Wireless
    ad4232e3e9 ar71xx: add unaligned access hacks for VXLAN
    074fa8dfe4 musl: move BUILD_DIR_TOOLCHAIN/musl symlink to configure step
    13a5cb9d2b musl: allow autorebuild
    166741240a mac80211: mesh: drop frames appearing to be from us
    21d3643828 ramips: add support for Widora Neo 32MB flash revision
    a411881a40 ramips: add flash size postfix to Widora neo
    6f425a28a4 kernel: generic: add 4.9 config option
    95ab18e012 vxlan: add options to enable and disable UDP checksums
    4d001af7c5 netifd: update to latest git HEAD
    c25d9cbde8 ramips: Fix GB-PC1 cpuclock again
    059b2f5481 ar71xx: fix MikroTik rb-nor-flash-16M-ac image
    a9ffe9fd75 procd: update to latest git HEAD
    9c4fe103cb ramips: add support for ZBT-WE1226
    b33c3e1b7c sunxi: Orange Pi R1: configure USB Ethernet controller.
    40db7a9dec sunxi: Orange Pi R1: Fix USB Ethernet and activate SPI
    75f7c1c9ac sunxi: use upstream patch for Orange Pi R1
    b1c82cbd51 sunxi: backport stmmac network patches
    ad2b3bf310 sunxi: Add support for kernel 4.14
    dda475ca30 sunxi: refresh kernel configuration
    eaf79d06b7 Revert "kernel: add IEEE-1284 parallel port support"
    24b0424ecb procd: update to latest git HEAD
    eb9e3651dd ar71xx: add support for the MikroTik RB911-2Hn/5Hn boards
    a4320b3332 ar71xx: make leds-gpio usable with single-ended GPIOs
    e379e60b17 ar71xx: mach-rbspi: return rb_info from rbspi_platform_setup
    c7371cd1ee ar71xx: add definitions for RouterBOARD hardware option bits
    5bdbc10b1b uqmi: silence error on pin verification
    666e9cf222 kernel: add IEEE-1284 parallel port support
    36f1978a70 pistachio: make patches apply again
    4d649ba949 ipq806x: make patches apply again
    e390756eff x86: Add CONFIG_RETPOLINE to fix build
    4336efe14b kernel: use upstream patches for musl
    e3c43ade0b ubus: fix PKG_MIRROR_HASH
    a30791242b nftables: update to 0.8.1
    3d8040e04f libnftnl: update to 1.0.9
    f9aca01a53 kernel: backport fix for nftables on big Endian
    e787ad5c96 tools/tar: update to 1.30
    a30370bbf1 kernel: bump 4.4 to 4.4.112
    d8565a06dc kernel: bump 4.9 to 4.9.77
    9ddfac8015 kernel: bump 4.14 to 4.14.14
    02050f7e7d kernel/4.{4, 9}: add manufacturer ID for Winbond NANDs
    adaf1cbcc8 dnsmasq: backport validation fix in dnssec security fix
    a3198061f8 dnsmasq: backport dnssec security fix

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=9c2ac19b032c81a454f1efbcf5681b80cad2fa39
+OPENWRT_COMMIT=d58c8f4029fa2c214454d7a5229e4964cbbc8fe1
 MAKE_ARGS=

7c5fedf2810e9c56d707d9a5d3286b1875ef3a0e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-02-01 11:17:38 +0100

    update to HEAD of OpenWRT-master (31. Jan 2018)
    * enable TL_WR842v2 again
    * 802.11s use 1Mbps for compatibility
    * use gcc 7.3.0
    
    3f7908d697 ar71xx-generic: enable ATH79_MACH_TL_WR841N_V8 machine
    33e8f7ee49 netfilter: add missing dependency to kmod-ipt-tproxy
    bf1032d71f mac80211: replace revert for 11s compatiblity with upstream fix
    352c74fcb4 netfilter: add packages for arp and bridge tables of nftables
    bbef76f1b1 nftables: remove dependency on kmod-nf-nat
    e7e025426a netfilter: clean up dependencies of kernel modules
    aa66aa0c9a scripts/qemustart: more portable array operation
    c9c2e4d78d openssl: remove call to now absent clean-staging make target
    f65591f113 kernel: add test MTD driver package
    a9c65c22a1 netdevices.mk: add missing dependency to kmod-hwmon-core
    b81b18b5a4 netdevices.mk: add hwmon to IGB and IXGBE drivers
    d76ad1dc79 lantiq: ase: turn off fpu emulator in default build
    82ceb2ad2a build: add config option KERNEL_MIPS_FPU_EMULATOR
    1d9296dcdb curl: bump to 7.58.0
    74beb6f710 Revert "netdevices.mk: add hwmon to IGB and IXGBE drivers"
    d4f539030b Revert "netdevices.mk: add missing dependency to kmod-hwmon-core"
    60ad837bea procd: fix procd_lock() when prepare_roofs
    53f62bc5e5 netdevices.mk: add missing dependency to kmod-hwmon-core
    bcd17ce9a3 binutils: assertion failure bfd/elfxx-mips.c:3860
    1a8523c835 toolchain: gcc: update 7.x to 7.3.0
    241e6dd3e9 build: cleanup SSP_SUPPORT configure option
    55779569eb toolchain: add gcc configure default PIE and SSP
    df0bd42fde build: add hardened builds with PIE (ASLR) support
    ca7e8627db kernel-headers: adjust PKG_ variables when using git clone method
    953bf6b43d kernel.mk: update LINUX_VERSION filename for cloned repo
    ab6a96f3f5 Config-devel.in: rename symbol KERNEL_GIT_BRANCH -> KERNEL_GIT_REF
    285791934b hostapd: add support for hostapd's radius_client_addr
    006a8a063c perf: use libunwind
    8590a5c06d libunwind: enable build for arm
    af707a178f netdevices.mk: add hwmon to IGB and IXGBE drivers
    a5199379c0 kernel/modules/other: disable Nokia BT UART
    e47fe3284f toolchain/arc: update to the most recent release arc-2017.09
    53021ce7f2 uClibc-ng: update to 1.0.28

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
 PACKAGES_LIST_DEFAULT=default tunnel-berlin backbone
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
-OPENWRT_COMMIT=d58c8f4029fa2c214454d7a5229e4964cbbc8fe1
+OPENWRT_COMMIT=3f7908d697bb04f82dfd68a86391efc94d5a7aec
 MAKE_ARGS=
